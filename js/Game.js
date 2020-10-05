53e0f4b9a92dfc0c0c5070804b3160e38aca3db6
Oliver-DA <71343538+Oliver-DA@users.noreply.github.com>
2020-10-04 22:47:45 -0400

    Update script.js

--- js/Game.js
+++ js/Game.js
@@ -2,39 +2,46 @@
  * Project 4 - OOP Game App
  * Game.js */
 
- class Game {
-     constructor () {
+ 
+
+
+class Game {
+    constructor () {
         this.missed = 0;
         this.phrases = ["basketball","Helo World","Point High","Running","Comming Home"];
         this.activePhrase = null;
-     }
+    }
 
-     getRandomPhrase () {
-         const randomIndex = Math.floor(Math.random() * this.phrases.length);
-         return this.phrases[randomIndex];
-     }
+    get getRandomPhrase () {
+        const randomIndex = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomIndex];
+    }
 
-     removeLife () {
+    removeLife () {
         const lives = document.querySelectorAll("#scoreboard ol li");
         lives[this.missed].firstElementChild.src = "images/lostHeart.png";
         this.missed += 1;
-        if(this.missed === 5){
-            setTimeout(this.gameOver,500);
-        }
 
-     }
+        this.missed === 5 ? setTimeout(this.gameOver,500) : null;
+    }
 
-     checkForWin () {
+    checkForWin () {
+
        const revealed = document.querySelectorAll("#phrase ul li.show");
        const space = document.querySelectorAll("#phrase ul li.space");
-       if(revealed.length+space.length === this.activePhrase.phrase.length){
-           this.gameOver("win");
-       }
-        
-     }
 
-     handleIteraction (e) {
-         let letter = e.target;
+        if (revealed.length+space.length === this.activePhrase.phrase.length) {
+            
+           setTimeout(() => {
+               this.gameOver("win")
+           },500)
+        }
+        
+    }
+
+    handleIteraction (e) {
+        let letter = e.target;
+
         if (e.target.tagName === "BUTTON"){
 
             if (this.activePhrase.checkLetter(letter.textContent)) {
@@ -47,26 +54,32 @@
             e.target.className = "wrong";
             e.target.disabled = true;
             this.removeLife();
+
         }
-     }
+    }
 
-     startGame () {
+    startGame () {
         document.getElementById('overlay').style.display = 'none';
-        this.activePhrase = new Phrase(this.getRandomPhrase());
+        this.activePhrase = new Phrase(this.getRandomPhrase);
         this.activePhrase.addPhraseToDisplay();
-        
-     }
+    }
 
-     gameOver (status = "lose") {
+    gameOver (status = "lose") {
         const overlay = document.getElementById('overlay');
-        overlay.style.display = "block"
+        overlay.style.display = "block";
+
         const message = overlay.querySelector("h1");
+
         if (status === "win") {
             message.className = "win";
             message.textContent = "Congrast! You have guessed the word!";
             return
+        } else {
+            message.className = "lose";
+            message.textContent = "You've run out of lives Good luck, next Time!";
+            return
         }
-        message.className = "lose";
-        message.textContent = "You've run out of lives Good luck, next Time!"
-     }
- } 
+        
+
+    }
+} 
--- js/app.js
+++ js/app.js
@@ -2,14 +2,33 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const game = new Game();
+function reset () {
+    document.querySelector('#phrase ul').innerHTML = "";
+    const keys = document.querySelectorAll(".keyrow button");
+    const lives = document.querySelectorAll("#scoreboard ol li");
+
+    keys.forEach(k => {
+        k.className = "key",
+        k.disabled = false;
+        return k
+    });
+
+    lives.forEach(live => live.firstElementChild.src = "images/liveHeart.png")
+}
+
+let game;
 const startButton = document.getElementById("btn__reset");
 const keyBoard = document.getElementById("qwerty");
 
-startButton.addEventListener('click',() => {   
+startButton.addEventListener('click',() => {
+    game = new Game();
+    reset();
     game.startGame();
+
+
 });
 
 keyBoard.addEventListener("click",e => {
     game.handleIteraction(e)
 })
+
